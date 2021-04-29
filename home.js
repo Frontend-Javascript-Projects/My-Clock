42fc88b2bffe0d0d518a5940f0eb1a5ced74d8fe
JimmyShukurow <62385133+JimmyShukurow@users.noreply.github.com>
2021-04-29 23:29:21 +0300

    background color changing every second

--- home.js
+++ home.js
@@ -2,6 +2,7 @@ setInterval(myFunction, 1000);
 
 function myFunction() {
     let date = new Date();
+    const colors = ['red','green','yellow','orange','violet','gray','tomato','dodgerblue','blue','brown']
 
     let degreeSeconds = date.getSeconds()*6;
     let degreeMinutes =date.getMinutes()*6;
@@ -9,8 +10,10 @@ function myFunction() {
     const seconds = document.querySelector(".seconds");
     const minutes = document.querySelector(".minutes");
     const hours = document.querySelector(".hours");
+    const body = document.querySelector("body");
     seconds.style.transform ='rotate('+degreeSeconds+'deg)'; 
     minutes.style.transform ='rotate('+degreeMinutes+'deg)'; 
     hours.style.transform ='rotate('+degreeHours+'deg)'; 
+    body.style.backgroundColor = colors[Math.floor(Math.random()*10)];
     console.log(Math.floor(Math.random()*10));
 }
