65b2fafe8d79bc80e192aff64dc4b5b53ca13ecf
JimmyShukurow <62385133+JimmyShukurow@users.noreply.github.com>
2021-04-30 15:10:20 +0300

    numbers are added

--- home.js
+++ home.js
@@ -16,3 +16,16 @@ function myFunction() {
     hours.style.transform ='rotate('+degreeHours+'deg)'; 
     body.style.backgroundColor = colors[Math.floor(Math.random()*10)];
 }
+
+function addNumbers(){
+    const numbers = document.querySelector(".numbers");
+    var radius = 4;
+    var hourNumbers = "";
+    for(let i = 1; i<=12; i ++){
+        let top = 5 - radius*Math.cos((30*Math.PI/180)*i)+"rem";
+        let left = 5 + radius*Math.sin((30*Math.PI/180)*i)+"rem";
+        hourNumbers += "<p style='top:"+top+"; left:"+left+"' >"+i+"</p>";
+    }
+    console.log(radius);
+    numbers.innerHTML = hourNumbers;
+};
